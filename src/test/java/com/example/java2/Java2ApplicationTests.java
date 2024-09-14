fe93c232695c57816ebccc6ba6d1ab9c02846f85
vladermik <vladermik@gmail.com~>
2024-09-14 18:12:34 +0300

    Initial commit

---
+++ src/test/java/com/example/java2/Java2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
